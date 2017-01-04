bd1b52e8d11571532d2ece91561f9c7237bfa210
ccwrc <j.muszelik@gmail.com>
2017-01-04 21:51:19 +0100

    add class user

---
+++ src/User.php
@@ -0,0 +1,94 @@
+<?php
+
+class User {
+    private $userAddress;
+    private $userEmail;
+    private $userId;
+    private $userName;
+    private $userPassword;
+    private $userSurname;
+    
+    public function __construct() {
+        $this->userAddress = "";
+        $this->userEmail = "";
+        $this->userId = -1;
+        $this->userName = "";
+        $this->userPassword = "";
+        $this->userSurname = "";
+    }
+    
+    public function getUserAddress() {
+        return $this->userAddress;
+    }
+    
+    public function getUserEmail() {
+        return $this->userEmail;
+    }
+    
+    public function getUserId() {
+        return $this->userId;
+    }
+    
+    public function getUserName() {
+        return $this->userName;
+    }
+    
+    public function getUserPassword() {
+        return $this->userPassword;
+    }
+    
+    public function getUserSurname() {
+        return $this->userSurname;
+    }
+    
+    public function setUserAddress($address) {
+        $address = htmlentities($address, ENT_QUOTES, "UTF-8");
+        if (is_string($address) && (strlen($address) <= 9000)) {
+            $this->userAddress = $address;
+            return $this;
+        } else {
+            return false;
+        }
+    }
+    
+    public function setUserEmail($email) {
+        $email = htmlentities($email, ENT_QUOTES, "UTF-8");
+        if (is_string($email) && (strlen($email) <= 250)) {
+            $this->userEmail = $email;
+            return $this;
+        } else {
+            return false;
+        }
+    }
+    
+    public function setUserName($name) {
+        $name = htmlentities($name, ENT_QUOTES, "UTF-8");
+        if (is_string($name) && (strlen($name) <= 250)) {
+            $this->userName = $name;
+            return $this;
+        } else {
+            return false;
+        }
+    }
+    
+    public function setUserPassword($password) {
+        $newHashedPassword = password_hash($password, PASSWORD_BCRYPT);
+        $this->userPassword = $newHashedPassword;
+        return $this;
+    }
+    
+    public function setUserSurname($surname) {
+        $surname = htmlentities($surname, ENT_QUOTES, "UTF-8");
+        if (is_string($surname) && (strlen($surname) <= 250)) {
+            $this->userSurname = $surname;
+            return $this;
+        } else {
+            return false;
+        }
+    }
+    
+    // reszta jutro...
+    
+
+    
+}
