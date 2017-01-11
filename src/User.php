67eb13d2d41885cb3e2b33ef8aa64fa10b047b24
ccwrc <j.muszelik@gmail.com>
2017-01-11 18:14:15 +0100

    add functions to class user

--- src/User.php
+++ src/User.php
@@ -100,11 +100,72 @@ class User {
                 return false;
             }
         } else {
-            // zrobić update oddzielnie
-        }
+            $statement = $conn->prepare("UPDATE User SET user_name = ?, user_email = ?, "
+                    . "user_password = ?, user_surname = ?, user_address = ? WHERE user_id = ?");
+            $statement->bind_param('sssssi', $this->userName, $this->userEmail, 
+                    $this->userPassword, $this->userSurname, $this->userAddress, $this->userId);
+            if ($statement->execute()) {
+                return true;
+            } else {
+                return false;
+            }
+        } 
     }
-
     
-
+    static public function loadUserById(mysqli $conn, $id){
+        $id = htmlentities($id, ENT_QUOTES, "UTF-8");
+        $id = $conn->real_escape_string($id);
+        
+        $sql = "SELECT * FROM User WHERE user_id = $id";
+        $result = $conn->query($sql);
+        
+        if($result && $result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        $loadedUser = new User();
+        $loadedUser->userId = $row['user_id'];
+        $loadedUser->userName = $row['user_name'];
+        $loadedUser->userPassword = $row['user_password'];
+        $loadedUser->userEmail = $row['user_email'];
+        $loadedUser->userSurname = $row['user_surname'];
+        $loadedUser->userAddress = $row['user_address'];
+        return $loadedUser;
+        } else {
+            return null;
+        }
+     }
+     
+    static public function loadUserByEmail(mysqli $conn, $email){
+        $email = htmlentities($email, ENT_QUOTES, "UTF-8");
+        $email = $conn->real_escape_string($email);
+        
+        $sql = "SELECT * FROM User WHERE user_email = '$email'";
+        $result = $conn->query($sql);
+        
+        if($result && $result->num_rows == 1) {
+        $row = $result->fetch_assoc();
+        $loadedUser = new User();
+        $loadedUser->userId = $row['user_id'];
+        $loadedUser->userName = $row['user_name'];
+        $loadedUser->userPassword = $row['user_password'];
+        $loadedUser->userEmail = $row['user_email'];
+        $loadedUser->userSurname = $row['user_surname'];
+        $loadedUser->userAddress = $row['user_address'];
+        return $loadedUser;
+        } else {
+            return null;
+        }
+     }
+     
+     static public function deleteUserFromDbById(mysqli $conn, $id) {
+         $id = htmlentities($id, ENT_QUOTES, "UTF-8");
+         $id = $conn->real_escape_string($id);
+         
+         $sql = "DELETE FROM User WHERE user_id = $id";
+         if ($result = $conn->query($sql)) {
+             return true;
+         } else {
+             return false;
+         }
+     }
     
 }
