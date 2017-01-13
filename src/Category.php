a50eca8068253789aa0533833a738ee096c3e0fb
ccwrc <j.muszelik@gmail.com>
2017-01-13 16:42:02 +0100

    add class category

---
+++ src/Category.php
@@ -0,0 +1,16 @@
+<?php
+
+class Category {
+    private $categoryId;
+    private $categoryName;
+    
+    public function __construct() {
+        $this->categoryId = -1;
+        $this->categoryName = "";
+    }
+    
+    
+    
+    
+}
+
