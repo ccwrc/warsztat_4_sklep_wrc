455652c81c90d5333cbd01166b2d0e89274a0a52
ccwrc <j.muszelik@gmail.com>
2017-01-16 20:44:59 +0100

    add admin pages

---
+++ admin/itemAdmin.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+
+?>
+
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title> przedmioty - administracja </title>
+<!--	
+	<meta name="description" content="krotki opis" />
+	<meta name="keywords" content="kluczowe" />
+	<meta name="author" content="WR & ccwrc">
+	
+	<link rel="stylesheet" href="css/style.css" type="text/css" />	
+	<script src="js/jquery-3.1.1.min.js"></script>
+        <script src="js/app.js"></script>	-->
+</head>
+
+<body>
+  <div class="container">
+	  
+    <div class="logo"> 
+    </div>
+
+    <div class="content">
+    <!-- Zarządzanie przedmiotami Administrator powinien mieć możliwość dodania
+    lub usunięcia przedmiotów. -->
+
+
+
+        <div class ="footer">
+            <br/><br/>
+            <a href="../index.php">Powrót do...</a> 
+        </div>      
+          
+    </div>
+  </div>  
+</body>
+</html>
+
+
---
+++ admin/orderAdmin.php
@@ -0,0 +1,45 @@
+<?php
+session_start();
+
+?>
+
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<title> zamówienia - administracja </title>
+<!--	
+	<meta name="description" content="krotki opis" />
+	<meta name="keywords" content="kluczowe" />
+	<meta name="author" content="WR & ccwrc">
+	
+	<link rel="stylesheet" href="css/style.css" type="text/css" />	
+	<script src="js/jquery-3.1.1.min.js"></script>
+        <script src="js/app.js"></script>	-->
+</head>
+
+<body>
+  <div class="container">
+	  
+    <div class="logo"> 
+    </div>
+
+    <div class="content">
+    <!-- Zarządzanie zamówieniami Administrator powinien widzieć wszystkie zamówienia (pogrupowane
+    po ich aktualnym statusie), móc zmienić status zamówienia, usunąć dane zamówienie i wysłać 
+    wiadomość do danego klienta na temat tego zamówienia. -->
+
+
+
+        <div class ="footer">
+            <br/><br/>
+	    <a href="../index.php">Powrót do...</a> 
+        </div>      
+          
+    </div>
+  </div>  
+</body>
+</html>
+
+
